7f422d0aa82ca3a8e279f36c1d64b818d4b71d70
Mikhail Istomin <redeemer@inbox.ru>
2018-11-12 23:25:21 +0300

    get ids

--- 1_get_ids/getIds.ts
+++ 1_get_ids/getIds.ts
@@ -1,8 +1,43 @@
 import { CONFIG } from "../config";
 import axios from "axios";
+import * as fs from "fs";
 
 const instance = axios.create({
-  baseURL: "https://api.twitch.tv/helix/videos?user_id=" + CONFIG.userId,
+  baseURL: "https://api.twitch.tv/helix/videos",
+  params: { user_id: CONFIG.userId },
   headers: { "Client-ID": CONFIG.twitchClientId }
 });
-instance.get("").then(x => console.log(x));
+
+async function main() {
+  let data = [];
+  let gen = asyncDataGenerator();
+  for await (let x of gen) {
+    data = data.concat(x);
+  }
+  fs.writeFileSync("1_get_ids/result.json", JSON.stringify(data));
+}
+
+async function getIds(cursor?: string) {
+  let response = cursor
+    ? await instance.get("", { params: { after: cursor } })
+    : await instance.get("");
+  let data = response.data;
+  return data.data && data.data.length ? data : null;
+}
+
+async function* asyncDataGenerator(): AsyncIterableIterator<any> {
+  let cursor = null;
+  let finish = false;
+  while (!finish) {
+    yield await getIds(cursor).then(d => {
+      if (d) {
+        cursor = d.pagination.cursor;
+        return d.data;
+      } else {
+        finish = true;
+      }
+    });
+  }
+}
+
+main();
