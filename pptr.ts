7ae6cbd085668315d9502f78dc99c0b3badfd6f2
Mikhail Istomin <redeemer@inbox.ru>
2018-11-21 23:49:19 +0300

    get screenshot on 50%

--- pptr.ts
+++ pptr.ts
@@ -2,23 +2,25 @@ import * as puppeteer from "puppeteer";
 import { CONFIG } from "./config";
 
 (async () => {
-  // const browser = await puppeteer.launch({
-  //   headless: false,
-  //   slowMo: 250,
-  //   // defaultViewport: {
-  //   //   width: 1000,
-  //   //   height: 1000
-  //   // },
-  //   // args: [
-  //   //   `--window-size=1000,1000`
-  //   // ],
-  // });
   const browser = await puppeteer.launch({
-    executablePath: CONFIG.chromePath
+    //headless: false,
+    executablePath: CONFIG.chromePath,
+    defaultViewport: {
+      width: 1920,
+      height: 1080
+    },
+    args: [`--window-size=1920,1080`]
   });
+  let url = "URL";
   const page = await browser.newPage();
-  await page.goto("URL");
-  await delay(10000);
+  await page.goto(url);
+  await delay(1000);
+  await page.goto(url);
+  await delay(3000);
+  page.click(".player-slider");
+  await delay(1000);
+  page.click(".video-info__container");
+  await delay(3000);
   await page.screenshot({ path: "example.png" });
 
   await browser.close();
